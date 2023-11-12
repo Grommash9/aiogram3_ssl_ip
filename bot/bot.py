b7e0c56878ffda1d0da701ec83f271efb1b8f913
Oleksandr <oleksandr.prudnikov@gigaclear.com>
2023-11-12 07:02:58 +0000

    bot to mysqlby local host

--- bot/bot.py
+++ bot/bot.py
@@ -16,7 +16,7 @@ from aiomysql import Connection, Cursor, DictCursor, connect
 from aiogram.types import User
 
 MYSQL = {
-    "host": "192.168.1.40",
+    "host": "127.0.0.1",
     "user": "root",
     "password": getenv("ROOT_PASSWORD"),
     "db": "support_bot_database",
